9ae0401d6760d35319005692966dcc60f274218f
supanut klayhan <supanut.kla@student.mahidol.ac.th>
2026-04-16 03:24:06 +0000

    yes

--- trackzero.js
+++ trackzero.js
@@ -11,7 +11,7 @@ app.use(router);
 
 router.get('/',(req,res) =>{
     console.log(`Request at: ${req.url}`);
-    res.send('Hello World in plain text');
+    res.sendFile(path.join(`${__dirname}/html/Home.html`));
 })
 router.get('/Login', (req, res) => {
     console.log('Req: /Login');
@@ -19,17 +19,23 @@ router.get('/Login', (req, res) => {
     res.sendFile(path.join(`${__dirname}/html/Login.html`));
 });
 
-router.get('/Member', (req, res) => {
-    console.log('Request at /Member');
-    res.sendFile(path.join(`${__dirname}/html/Member.html`));
+router.get('/Team', (req, res) => {
+    console.log('Request at /Team');
+    res.sendFile(path.join(`${__dirname}/html/Team.html`));
 });
-router.get('/html/Search.html', (req, res) => {
+router.get('/Search', (req, res) => {
     console.log('Request at /Search');
     res.sendFile(path.join(`${__dirname}/html/Search.html`));
     
 });
-
-
+router.get('/Manage-Products', (req, res) => {
+    console.log('Request at /Manage-Products');
+    res.sendFile(path.join(`${__dirname}/html/productservice-management.html`));
+});
+router.get('/Detail', (req, res) => {
+    console.log('Request at /Detail');
+    res.sendFile(path.join(`${__dirname}/html/Detail.html`));
+});
 app.listen(port, () => {
 console.log(`Server listening on port: ${port}`)
 });
